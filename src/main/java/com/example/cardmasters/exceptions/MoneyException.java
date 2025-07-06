ffd3f8276aa9fac8c08048a6eb5517d65d37d5ad
v.ionov <v.ionov@mycrg.ru>
2025-07-06 16:24:31 +0300

    all basic logic complete

---
+++ src/main/java/com/example/cardmasters/exceptions/MoneyException.java
@@ -0,0 +1,11 @@
+package com.example.cardmasters.exceptions;
+
+public class MoneyException extends RuntimeException{
+
+    public MoneyException(String message) {
+        super(message);
+    }
+
+    public MoneyException(String message, Throwable cause) {
+    }
+}
